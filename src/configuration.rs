ae4d2c0b2a0a35baaa3b31071d7c595f7127b8d3
Christos Ploutarchou <cploutarchou@gmail.com>
2023-10-22 22:21:55 +0300

    extend configuration class

--- src/configuration.rs
+++ src/configuration.rs
@@ -13,7 +13,7 @@ const JSON_SCHEMA_VALIDATION_KEYWORDS: [&str; 10] = [
     "minItems",
 ];
 
-struct Config<'a> {
+pub struct Configuration<'a> {
     base_path: &'a str,
     server_index: i32,
     server_operation_index: HashMap<&'a str, i32>,
@@ -25,9 +25,10 @@ struct Config<'a> {
     password: Option<&'a str>,
     access_token: Option<&'a str>,
     ssl_ca_cert: Option<&'a str>,
+    pub(crate) client_side_validation: bool,
 }
 
-impl<'a> Config<'a> {
+impl<'a> Configuration<'a> {
     fn new(
         base_path: &'a str,
         api_key: HashMap<&'a str, &'a str>,
@@ -37,7 +38,7 @@ impl<'a> Config<'a> {
         access_token: Option<&'a str>,
         ssl_ca_cert: Option<&'a str>,
     ) -> Self {
-        Config {
+        Configuration {
             base_path,
             server_index: 0,
             server_operation_index: HashMap::new(),
@@ -49,6 +50,25 @@ impl<'a> Config<'a> {
             password,
             access_token,
             ssl_ca_cert,
+            client_side_validation: true,
+        }
+    }
+
+    pub(crate) fn get_default() -> Self {
+        // Your default configuration implementation here
+        Configuration {
+            base_path: "", // Default value, can be changed
+            server_index: 0,
+            server_operation_index: HashMap::new(),
+            server_variables: HashMap::new(),
+            server_operation_variables: HashMap::new(),
+            api_key: HashMap::new(),
+            api_key_prefix: HashMap::new(),
+            username: None,
+            password: None,
+            access_token: None,
+            ssl_ca_cert: None,
+            client_side_validation: true,
         }
     }
 }
